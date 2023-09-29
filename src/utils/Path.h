446ce5b69e4a839a76f734f1e4b93647848a39a5
Caio Rosa <addminplayerbr1122@gmail.com>
2023-09-28 21:43:03 -0300

    feat: Criado util Path

---
+++ src/utils/Path.h
@@ -0,0 +1,15 @@
+//
+// Created by caior on 28/09/2023.
+//
+
+#ifndef BOMBERVALI_PATH_H
+#define BOMBERVALI_PATH_H
+
+#include <string>
+
+namespace Path {
+    std::string ROOT_DIR = "..";
+    std::string ASSETS_DIR = ROOT_DIR + "/assets";
+}
+
+#endif //BOMBERVALI_PATH_H
